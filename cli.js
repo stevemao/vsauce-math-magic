28b78118cd07cd9765e3ccc938daaa9c69bc5b71
Steve Mao <maochenyan@gmail.com>
2016-03-25 12:44:18 +1100

    linting

--- cli.js
+++ cli.js
@@ -18,8 +18,8 @@ var count = cli.input[0] || 10;
 var results = vsauceMathMagic(count);
 console.log(results);
 var finishTime = new Date();
-console.log('The computer has shuffled '
-	+ green(results.length) + ' times and it took '
-	+ green(finishTime - startTime)
-	+ ' milliseconds to get the same order of the deck that has appear before when there are '
-	+ green(count) + ' cards in the deck.');
+console.log('The computer has shuffled ' +
+	green(results.length) + ' times and it took ' +
+	green(finishTime - startTime) +
+	' milliseconds to get the same order of the deck that has appear before when there are ' +
+	green(count) + ' cards in the deck.');
